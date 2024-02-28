eb421f66a637f2137d351dc3ac80ddad906c3e3c
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-02-29 01:16:18 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,8 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation ("androidx.navigation:navigation-compose:2.7.7")
     implementation ("androidx.compose.foundation:foundation-layout:1.6.2")
+    implementation ("com.google.code.gson:gson:2.10")
+    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
