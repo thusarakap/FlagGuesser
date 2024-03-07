e5903ebe3a602232f924767f8d4440c19b746b09
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-03-08 03:19:45 +0530

    Update MainActivity.kt

--- app/src/main/java/com/thusarakap/flagguesser/MainActivity.kt
+++ app/src/main/java/com/thusarakap/flagguesser/MainActivity.kt
@@ -5,34 +5,52 @@
 package com.thusarakap.flagguesser
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.*
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -42,9 +60,6 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.thusarakap.flagguesser.ui.theme.FlagGuesserTheme
 import kotlinx.coroutines.delay
-import org.json.JSONObject
-import java.io.BufferedReader
-import java.io.InputStreamReader
 
 
 class MainActivity : ComponentActivity() {
@@ -172,13 +187,13 @@ fun getFlagImageByCountryCode(countryCode: String): Int {
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun Screen1(navController: NavHostController) {
-    var selectedCountry by remember { mutableStateOf("") }
-    var countryNames by remember { mutableStateOf<List<String>>(emptyList()) }
-    var flagCountryCode by remember { mutableStateOf("") }
-    var isCorrect by remember { mutableStateOf<Boolean?>(null) }
-    var correctCountry by remember { mutableStateOf("") }
-    var wasPopupShown by remember { mutableStateOf(false) }
-    var resultPopupShown by remember { mutableStateOf(false) }
+    var selectedCountry by rememberSaveable { mutableStateOf("") }
+    var countryNames by rememberSaveable { mutableStateOf<List<String>>(emptyList()) }
+    var flagCountryCode by rememberSaveable { mutableStateOf("") }
+    var isCorrect by rememberSaveable { mutableStateOf<Boolean?>(null) }
+    var correctCountry by rememberSaveable { mutableStateOf("") }
+    var wasPopupShown by rememberSaveable { mutableStateOf(false) }
+    var resultPopupShown by rememberSaveable { mutableStateOf(false) }
 
     LaunchedEffect(Unit) {
         countryNames = loadCountryNames()
@@ -331,6 +346,20 @@ fun loadCountryNames(): List<String> {
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun Screen2(navController: NavHostController) {
+    var countryCode by rememberSaveable { mutableStateOf("") }
+    var countryName by rememberSaveable { mutableStateOf("") }
+    var dashes by rememberSaveable { mutableStateOf<String>("") }
+    var userInput by rememberSaveable { mutableStateOf("") }
+
+    LaunchedEffect(Unit) {
+        // Generate a random flag country code and corresponding country name
+        countryCode = getRandomFlagCountryCode()
+        countryName = getCountryNameByCountryCode(countryCode)
+
+        // Initialize the dashes string with spaces between each dash
+        dashes = countryName.map { if (it.isWhitespace()) "  " else "- " }.joinToString("")
+    }
+
     Scaffold(
         topBar = { TopBar(navController) },
         content = {
@@ -339,14 +368,39 @@ fun Screen2(navController: NavHostController) {
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text("Guess-Hints")
+                // Flag Image
+                FlagImage(countryCode = countryCode, modifier = Modifier.size(225.dp))
                 Spacer(modifier = Modifier.height(16.dp))
-                Button(onClick = { /* submit action */ }) {
+
+                // Dashes representing the country name
+                Text(text = dashes, style = TextStyle(fontSize = 24.sp))
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Textbox for user input
+                OutlinedTextField(
+                    value = userInput,
+                    onValueChange = { userInput = it },
+                    label = { Text("Enter a character") },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Submit Button
+                Button(onClick = { handleSubmit(userInput) }) {
                     Text("Submit")
                 }
             }
         }
     )
+}
+
+fun handleSubmit(userInput: String) {
+    // Handle the submission logic here
+    // Update the dashes string based on the user input
+    // Compare userInput with the country name and update dashes if necessary
+    // For example, if userInput is 'C' and the country name is 'Canada',
+    // update dashes to 'C-----'
 }
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
